933651c682b716c0f2e84b380da390b0b5fc3d60
ife-codes <ife4373@gmail.com>
2025-04-23 11:10:19 +0100

    learnt select and insert

--- app.js
+++ app.js
@@ -1,23 +1,34 @@
 const express = require("express");
 const app = express();
 
-require("dotenv").config()
-const morgan = require("morgan")
+require("dotenv").config();
+const morgan = require("morgan");
 
 const _supabase = require("./supabaseConfig");
 
 app.use(express.json());
-app.use(morgan("dev"))
+app.use(morgan("dev"));
 
-app.listen(3000)
+app.listen(3000);
 
-
-app.get("/", (req, res) => {});
-
+app.get("/", async(req, res) => {
+  try {
+    const { data, error } = await _supabase.from("links").select("*");
+    if (error) {
+      res.status(500).json({ error: error.message });
+    } else {
+      res.status(200).json(data);
+      console.log(data);
+      
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 app.post("/", async (req, res) => {
   const { title, url } = req.body;
   try {
-    const {data, error} = await _supabase.from("links").insert([
+    const { data, error } = await _supabase.from("links").insert([
       {
         title: title,
         url: url,
@@ -25,8 +36,8 @@ app.post("/", async (req, res) => {
     ]);
     if (error) return res.status(500).json({ error: error.message });
 
-    return res.status(201).json(data[0])
+    return res.status(201).json(data[0]);
   } catch (error) {
-    res.status(500).json({ error: error })
+    res.status(500).json({ error: error });
   }
 });
